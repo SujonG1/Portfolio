1584ed316d5c06ef4769e0d6b515e568ff1c6593
Sujon Ganguly <sujonganguly01@gmail.com>
2026-06-14 16:49:30 +0530

    Refactor particle movement and drawing logic

--- js/particles.js
+++ js/particles.js
@@ -3,8 +3,6 @@ import { randomBetween, lerp, clamp } from './utils.js';
 class Particle {
   constructor(canvas) {
     this.canvas = canvas;
-    this.angle = Math.random() * Math.PI * 2;
-    this.speed = Math.random() * 0.02;
     this.reset();
   }
 
@@ -19,20 +17,18 @@ class Particle {
   }
 
   randomColor() {
-  const colors = [
-    [108, 99, 255],
-    [0, 245, 255],
-    [255, 79, 154]
-  ];
+    const colors = [
+      [108, 99, 255],
+      [0, 245, 255],
+      [255, 79, 154]
+    ];
 
-  return colors[Math.floor(Math.random() * colors.length)];
-}
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
 
   update(mouse) {
-    this.angle += this.speed;
-
-    this.x += Math.cos(this.angle) * 0.15;
-    this.y += Math.sin(this.angle) * 0.15;
+    this.x += this.vx;
+    this.y += this.vy;
 
     // Mouse repulsion
     const dx = mouse.x - this.x;
@@ -55,14 +51,15 @@ class Particle {
   }
 
   draw(ctx) {
-  ctx.beginPath();
-  ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
 
-  const [r,g,b] = this.color;
-  ctx.fillStyle = `rgba(${r},${g},${b},${this.alpha})`;
+    const [r, g, b] = this.color;
 
-  ctx.fill();
-  }
+    ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${this.alpha})`;
+
+    ctx.fill();
+}
 }
 
 export function initParticleSystem() {
@@ -100,11 +97,13 @@ export function initParticleSystem() {
         const dy = particles[i].y - particles[j].y;
         const d = Math.sqrt(dx * dx + dy * dy);
 
-        if (d < 120 && d > 0) {
-          const force = (120 - d) / 120;
-
-          this.vx -= (dx / d) * force * 0.02;
-          this.vy -= (dy / d) * force * 0.02;
+        if (d < 100) {
+          ctx.beginPath();
+          ctx.moveTo(particles[i].x, particles[i].y);
+          ctx.lineTo(particles[j].x, particles[j].y);
+          ctx.strokeStyle = `rgba(108, 99, 255, ${(1 - d / 100) * 0.12})`;
+          ctx.lineWidth = 0.5;
+          ctx.stroke();
         }
       }
     }
